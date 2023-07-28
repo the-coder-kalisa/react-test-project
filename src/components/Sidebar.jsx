7aee6a8c9d23ba5b3e3fa5f3be8bd63919bfd31e
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2023-07-28 11:09:55 +0200

    [add] added responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { styled } from "styled-components";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const SidebarWrapper = styled.div`
   border-radius: 10px;
@@ -11,6 +12,15 @@ const SidebarWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1.2rem;
+`;
+
+const NotMdSidebarWrapper = styled(SidebarWrapper)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  height: 100vh;
+  width: 100vw;
 `;
 
 const SidebarButton = styled.button`
@@ -43,29 +53,79 @@ const Sidebar = () => {
 
   const [activeNav, setActiveNav] = useState("My Agents");
 
-  // const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const [md, setMd] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth > 768) {
+      setMd(true);
+    } else {
+      setMd(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <SidebarWrapper>
-      <CloseSidebarButton>
-        Close <br />
-        Sidebar
-      </CloseSidebarButton>
-      {navs.map((nav, index) => (
-        <Nav
-          onClick={() => setActiveNav(nav)}
-          style={
-            nav === activeNav
-              ? { background: "#757575", color: "white" }
-              : { background: "white", color: "black" }
-          }
-          $active
-          key={index}
-        >
-          {nav}
-        </Nav>
-      ))}
-    </SidebarWrapper>
+    <>
+      {!open ? (
+        <AiOutlineMenu size={20} onClick={() => setOpen(true)} />
+      ) : !md ? (
+        <NotMdSidebarWrapper>
+          <CloseSidebarButton
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            Close <br />
+            Sidebar
+          </CloseSidebarButton>
+          {navs.map((nav, index) => (
+            <Nav
+              onClick={() => setActiveNav(nav)}
+              style={
+                nav === activeNav
+                  ? { background: "#757575", color: "white" }
+                  : { background: "white", color: "black" }
+              }
+              $active
+              key={index}
+            >
+              {nav}
+            </Nav>
+          ))}
+        </NotMdSidebarWrapper>
+      ) : (
+        <SidebarWrapper>
+          <CloseSidebarButton
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            Close <br />
+            Sidebar
+          </CloseSidebarButton>
+          {navs.map((nav, index) => (
+            <Nav
+              onClick={() => setActiveNav(nav)}
+              style={
+                nav === activeNav
+                  ? { background: "#757575", color: "white" }
+                  : { background: "white", color: "black" }
+              }
+              $active
+              key={index}
+            >
+              {nav}
+            </Nav>
+          ))}
+        </SidebarWrapper>
+      )}
+    </>
   );
 };
 
